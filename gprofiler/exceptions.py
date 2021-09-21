87b6c03a4fa7a21849e1baad6dfe30b79b850260
Lior Neumann <32582483+liornm@users.noreply.github.com>
2021-09-21 15:01:10 +0300

    a

--- gprofiler/exceptions.py
+++ gprofiler/exceptions.py
@@ -49,4 +49,9 @@ class StateAlreadyInitializedException(Exception):
 
 
 class BadResponseCode(Exception):
+    def __init__(self, response_code: int):
+        super().__init__(f"Got a bad HTTP response code {response_code}")
+
+
+class ThreadStopTimeoutError(Exception):
     pass
